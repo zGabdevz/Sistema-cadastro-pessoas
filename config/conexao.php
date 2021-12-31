fb6a09c74afb20e68b56367c9d0144d06fdef2e3
unknown <srgamerbryt@gmail.com>
2021-12-30 22:09:58 -0200

    arquivo de conexao

--- config/conexao.php
+++ config/conexao.php
@@ -1,3 +1,3 @@
 <?php
 #código de conexão ao banco de dados
-$conn = new Pessoa('epiz_30151582_dabnk','sql211.epizy.com','epiz_30151582', 'hHU6ifrHC3BqF');
+$conn = new Pessoa('db_name','localhost','root', 'IsThereAny?');
